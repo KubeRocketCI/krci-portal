6fd7c6d858d1b982cb99a1e6306f725dd39b5a24
Vladyslav Palyvoda <vladyslav_palyvoda@epam.com>
2025-10-30 17:59:06 +0200

    fix: Post migration fixes 2 (#1)

--- apps/client/src/modules/platform/pipelines/pages/list/components/PipelineFilter/constants.ts
+++ apps/client/src/modules/platform/pipelines/pages/list/components/PipelineFilter/constants.ts
@@ -1,16 +1,18 @@
 import { MatchFunctions } from "@/core/providers/Filter";
-import { Pipeline } from "@my-project/shared";
+import { Pipeline, pipelineLabels } from "@my-project/shared";
 import { PipelineListFilterValues } from "./types";
 import { createSearchMatchFunction } from "@/core/providers/Filter";
 
 export const PIPELINE_LIST_FILTER_NAMES = {
   SEARCH: "search",
   NAMESPACES: "namespaces",
+  PIPELINE_TYPE: "pipelineType",
 } as const;
 
 export const pipelineFilterDefaultValues: PipelineListFilterValues = {
   [PIPELINE_LIST_FILTER_NAMES.SEARCH]: "",
   [PIPELINE_LIST_FILTER_NAMES.NAMESPACES]: [],
+  [PIPELINE_LIST_FILTER_NAMES.PIPELINE_TYPE]: "all",
 };
 
 export const matchFunctions: MatchFunctions<Pipeline, PipelineListFilterValues> = {
@@ -19,5 +21,12 @@ export const matchFunctions: MatchFunctions<Pipeline, PipelineListFilterValues> 
     const arrayValue = Array.isArray(value) ? value : value ? [value] : [];
     if (arrayValue.length === 0) return true;
     return arrayValue.includes(item.metadata.namespace!);
+  },
+  [PIPELINE_LIST_FILTER_NAMES.PIPELINE_TYPE]: (item, value) => {
+    if (value === "all") {
+      return true;
+    }
+
+    return item?.metadata?.labels?.[pipelineLabels.pipelineType] === value;
   },
 };
